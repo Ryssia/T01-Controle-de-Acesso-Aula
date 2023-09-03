718a397b48c7afe0c74cd5c3a92cb39d18c8ea8e
diegohoss <diego.hoss@ifpr.edu.br>
2023-09-03 17:29:35 -0300

    api-token-metodo-patch-ok

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\Api\NoticiaController;
 use App\Models\Noticia;
+use App\Models\User;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,11 +25,33 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //Route::get('/noticias', 'App\Http\Controllers\Api\NoticiaController@index');
 Route::apiResource('noticias', NoticiaController::class);
-Route::patch('/noticias/{noticia}', function(Noticia $noticia, Request $request){
-    $noticia->titulo = $request->titulo;
-    $noticia->save();
-    return $noticia;
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::patch('/noticias/{noticia}', function(Noticia $noticia, Request $request){
+        $noticia->titulo = $request->titulo;
+        $noticia->save();
+        return $noticia;
+    });
 });
+
+Route::post('/login', function(Request $request){
+
+        $credenciais = $request->only(['name', 'email', 'password']);
+        
+        if(Auth::attempt($credenciais) === false){
+            return response()->json('Nao Autorizado', 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('token');
+        //dd($token);
+
+        return response()->json(['token' => $token->plainTextToken]);
+        
+        
+
+});
+
 /*
 Route::get('/noticias', function(){
     return response()->json(Noticia::all());
