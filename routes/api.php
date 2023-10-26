1cb47d311cf585bd580a7f67c3f026007b9b812e
Ryssia <ryssia.elias@gmail.com>
2023-10-26 16:37:00 -0300

    login e token

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Noticia;
 use App\Http\Controllers\Api\NoticiaController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +27,19 @@ Route::apiResource('noticias', NoticiaController::class);
 Route::patch('/noticias/{noticia}', function(Request $request, Noticia $noticia){
     $noticia->titulo = $request->titulo;
     $noticia->save();
-    
+
     return response()->json($noticia,200);
+});
+
+Route::post('/login', function(Request $request){
+    $credenciais = $request->only(['name', 'email', 'password']);
+
+    if(Auth::attempt($credenciais) === false){
+        return response()->json('credenciais invalidas', 401);
+    }
+
+    $user = Auth::user();
+    //$user->tokens()->delete();
+    $token = $user->createToken('token');
+    return response()->json(['token' => $token->plainTextToken]);
 });
